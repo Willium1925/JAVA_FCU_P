cd484ce42e4489cd9ff94d2413c426563543884f
me <D1397026@o365.fcu.edu.tw>
2025-02-25 16:06:37 +0800

    little change

--- src/ArithmeticGUI.java
+++ src/ArithmeticGUI.java
@@ -2,7 +2,7 @@
 
 public class ArithmeticGUI {
     public static void main(String[] argv) {
-        for(int i =0 ; i<=3 ; i++) {
+        for(int i =0 ; i<=4 ; i++) {
             System.out.println(i);
         }
     }
