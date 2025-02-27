dc1db4bd3ed9dfe9ecbcdd2bdac2e7362844acd2
Willium1925 <D1397026@o365.fcu.edu.tw>
2025-02-27 15:46:12 +0800

    釐清scanner相關方法

--- src/com/lee/lesson_saple/basic/HomeWork_0227.java
+++ src/com/lee/lesson_saple/basic/HomeWork_0227.java
@@ -8,7 +8,7 @@ public class HomeWork_0227 {
 //        假設某個自動售票機只能接受10元、5元、1元的硬幣。
 //        請設計一程式，當使用者輸入一小於999的正整數數字時，他所需投入各種幣值硬幣最少的數量分別是多少？若數字大於999則顯示：輸入過大。
 //        例如
-//        使用者輸入：123，則程式顯示：10元12個，5元0個，2元3個。
+//        使用者輸入：123，則程式顯示：10元12個，5元0個，1元3個。
           /*
             n=1x+5y+10z
             n/10=z
@@ -34,12 +34,18 @@ public class HomeWork_0227 {
 //        請撰寫一程式，計算出輸入之整數除以13的商數與餘數。
 //        例如：
 //        輸入26，輸出商數=2、餘數=0。
-//        System.out.println("請輸入任意正整數");
-//        Scanner s02 = new Scanner(System.in);
-//        int userInsert02 = s02.nextInt();
-//        int a = userInsert02/13;
-//        int b = userInsert02%13;
-//        System.out.println(userInsert02 + "除以13" + "，商數是" + a + "，餘數是" + b );
+        /*
+        * 是否有輸入
+        * 輸入是否為單純空白或太薄
+        * 不論是否前面有空白都能讀取，但判斷是否為數字
+        * 是否為正整數*/
+        System.out.println("請輸入任意正整數");
+        Scanner s02 = new Scanner(System.in);
+
+        int userInsert02 = s02.nextInt();
+        int a = userInsert02/13;
+        int b = userInsert02%13;
+        System.out.println(userInsert02 + "除以13" + "，商數是" + a + "，餘數是" + b );
 
 
 
@@ -55,24 +61,24 @@ public class HomeWork_0227 {
 //        2*c+2*r=2*headNum;
 //        footNum-2*headNum=2*r;
 
-        System.out.println("有多少隻腳？");
-        Scanner s03 = new Scanner(System.in);
-        int footNum = s03.nextInt();
-        //s03.next(); // 為何不用打這行？
-        System.out.println("有多少顆頭？");
-        int headNum = s03.nextInt();
-
-        int r = (footNum-2*headNum)/2;
-        int c = headNum - r;
-        String strCR = "總共有" + c + "隻雞，" + r + "隻兔子";
-
-        if (headNum>footNum || r<0 || c<0) {
-            System.out.println("該數據無解，你是否遭遇The THING");
-        } else if ((footNum-2*headNum)%2 == 0) {
-            System.out.println(strCR);
-        } else {
-            System.out.println("該數據無解");
-        }
+//        System.out.println("有多少隻腳？");
+//        Scanner s03 = new Scanner(System.in);
+//        int footNum = s03.nextInt();
+//        //s03.next(); // 為何不用打這行？
+//        System.out.println("有多少顆頭？");
+//        int headNum = s03.nextInt();
+//
+//        int r = (footNum-2*headNum)/2;
+//        int c = headNum - r;
+//        String strCR = "總共有" + c + "隻雞，" + r + "隻兔子";
+//
+//        if (headNum>footNum || r<0 || c<0) {
+//            System.out.println("該數據無解，你是否遭遇The THING");
+//        } else if ((footNum-2*headNum)%2 == 0) {
+//            System.out.println(strCR);
+//        } else {
+//            System.out.println("該數據無解");
+//        }
 
 
 //        請設計一程式，讓使用者輸入任意整數，程式判斷該數字為奇數或偶數。
