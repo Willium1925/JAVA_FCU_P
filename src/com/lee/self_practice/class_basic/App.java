b63ee2dacead55935dfca41a244a26d75e80c69e
Willum1925 <D1397026@o365.fcu.edu.tw>
2025-03-03 23:49:15 +0800

    自定義方法、參數，測試 好像卡在跟return，或是把void改成其他，的地方

--- src/com/lee/self_practice/class_basic/App.java
+++ src/com/lee/self_practice/class_basic/App.java
@@ -10,18 +10,23 @@ class Test01 {
 class Test02 {
     static int a = 20;
     public static void main(String[] args) {
-        int a = 30;
-        System.out.println(Test01.a);
-        System.out.println(Test02.a);
-
-        Poeple01.talk();
-        Poeple01.talk_advanced("進階說話");
-        Poeple01.talk_advanced("請輸入想說的話:");
-        Scanner sca = new Scanner(System.in);
+//        int a = 30;
+//        System.out.println(Test01.a);
+//        System.out.println(Test02.a);
+//
+//        Poeple01.talk();
+//        Poeple01.talk_advanced("進階說話");
+//        Poeple01.talk_advanced("請輸入想說的話:");
+//        Scanner sca = new Scanner(System.in);
         //System.out.println(sca.nextLine());
-        Poeple01.talk_advanced(sca.nextLine());
+        //Poeple01.talk_advanced(sca.nextLine());
 
-        BasicMath.counter(6,7);
+        //BasicMath.counter(6,7);
+        //BasicMath.counter((Integer.parseInt(Poeple01.talk_advanced(sca.nextLine()))), (Integer.parseInt(Poeple01.talk_advanced(sca.nextLine()))));
+
+        System.out.println("請輸入第一個數字");
+        Scanner sca003 = new Scanner(System.in);
+        int x = Integer.parseInt(Poeple01.talk_advanced(sca003.nextLine()));
     }
 }
 
