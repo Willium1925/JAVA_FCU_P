90e19db62ab34cc15f15bc446ed06a75430e3506
Willum1925 <D1397026@o365.fcu.edu.tw>
2025-03-03 23:20:05 +0800

    自定義方法、參數，測試

--- src/com/lee/self_practice/class_basic/App.java
+++ src/com/lee/self_practice/class_basic/App.java
@@ -1,13 +1,33 @@
 package com.lee.self_practice.class_basic;
 
+import java.util.Scanner;
+
 class Test01 {
     static int a = 10;
     static  String s = "aaa222";
 }
 
 class Test02 {
-    static int b = 20;
+    static int a = 20;
     public static void main(String[] args) {
+        int a = 30;
+        System.out.println(Test01.a);
+        System.out.println(Test02.a);
+        Test03.talk();
+        Test03.talk_advanced("進階說話");
+        Test03.talk_advanced("請輸入想說的話:");
+        Scanner sca = new Scanner(System.in);
+        //System.out.println(sca.nextLine());
+        Test03.talk_advanced(sca.nextLine());
+    }
+}
 
+class Test03 {
+    static void talk(){
+        System.out.println("單純說話");
+    }
+
+    static void talk_advanced(String string){
+        System.out.println(string);
     }
 }
