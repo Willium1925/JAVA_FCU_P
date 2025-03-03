aa1a6f7dc7dd46066442c0b4406d5611d4ea45cc
Willum1925 <D1397026@o365.fcu.edu.tw>
2025-03-03 22:37:12 +0800

    類別操作

---
+++ src/com/lee/self_practice/class_basic/App.java
@@ -0,0 +1,13 @@
+package com.lee.self_practice.class_basic;
+
+class Test01 {
+    static int a = 10;
+    static  String s = "aaa222";
+}
+
+class Test02 {
+    static int b = 20;
+    public static void main(String[] args) {
+
+    }
+}
