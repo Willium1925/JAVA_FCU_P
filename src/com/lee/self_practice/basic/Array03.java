d7e84b487522da9d628cff713f29ae6342849b06
Willum1925 <D1397026@o365.fcu.edu.tw>
2025-03-12 22:52:39 +0800

    nothing

---
+++ src/com/lee/self_practice/basic/Array03.java
@@ -0,0 +1,12 @@
+package com.lee.self_practice.basic;
+
+public class Array03 {
+    public static void main(String[] args) {
+        findMaxMin();
+    }
+
+    static void findMaxMin() {
+        int[] temp = new int[]{10, 50, 68, 49, 36, 51, 78};
+
+    }
+}
