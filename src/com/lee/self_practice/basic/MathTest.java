714258fc7092baeccf083a6a7c843d2f1df9a42b
Willum1925 <D1397026@o365.fcu.edu.tw>
2025-03-12 20:46:32 +0800

    一些int issue

---
+++ src/com/lee/self_practice/basic/MathTest.java
@@ -0,0 +1,22 @@
+package com.lee.self_practice.basic;
+
+public class MathTest {
+    public static void main(String[] args) {
+        double a = 9.87;
+        System.out.println(Math.rint(a));
+        int aa = (int)a;
+        System.out.println(aa);
+
+    }
+
+    static void IntTest() {
+        int a = 6;
+        Integer aNew = new Integer(6);
+        int aParse = Integer.parseInt("6");
+        int aValue01 = Integer.valueOf("6");
+        int aValue02 = Integer.valueOf(6);
+        int aValue03 = Integer.valueOf(6);
+
+
+    }
+}
