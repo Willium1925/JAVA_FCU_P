1f0be58449a3839758298eedd2317f11a36a1d14
Willium1925 <d1397026@o365.fcu.edu.tw>
2025-03-12 11:52:40 +0800

    陣列常用方法

--- src/com/lee/self_practice/basic/Array01.java
+++ src/com/lee/self_practice/basic/Array01.java
@@ -4,7 +4,8 @@ import java.util.Arrays; // 重要！
 
 public class Array01 {
     public static void main(String[] args) {
-    MemoryCheck();
+        MemoryCheck();
+        ArrayMethods();
 
     }
 
@@ -16,4 +17,57 @@ public class Array01 {
         int[][] score02 = new int[][]{{10, 20}, {20, 30}, {10, 20},{30, 40}};
         //System.out.println((score02[0]).getClass);
     }
+
+    static void ArrayMethods() {
+        int[] array00 = { 1, 1, 2, 3, 5, 8, 13 };
+        System.out.println(Arrays.toString(array00));
+
+        // 快速的複製一個一模一樣的資料的陣列物件，與原陣列佔用不同記憶體，彼此獨立。
+        int[] array01 = { 1, 1, 2, 3, 5, 8, 13 };
+        int[] arrayClo=array01.clone();
+        array01[0]=555;
+        System.out.println(Arrays.toString(array01));
+        System.out.println(Arrays.toString(arrayClo));
+
+        // 可以把陣列全部用value填滿，常用於陣列初始化。Arrays.fill(Object[],value)
+        int[] arrayFill = new int[10];
+        System.out.println(Arrays.toString(arrayFill));
+        Arrays.fill(arrayFill, -1);  // 以 -1 填滿 arrayFill
+        System.out.println(Arrays.toString(arrayFill));
+
+        // 陣列排序 Arrays.sort(Object[])
+        // 這個方法超美妙的，可以把陣列從小排到大
+        // 內部實做是快速排序法(quick sort)，時間複雜度 O(NlogN)，
+        // 比較的機制是該物件的 .compareTo()方法，實做上可以省掉很多寫排序演算法的時間。
+        int[] array03 = {50,1,-999,6000,77,3,50,0};
+        System.out.println(Arrays.toString(array03));
+        Arrays.sort(array03);
+        System.out.println(Arrays.toString(array03));
+
+        // 陣列搜尋 Arrays.binarySearch(Object[],key)
+        // 利用binarySearch搜尋陣列中目標key的索引值，
+        // 時間複雜度為O(logN)，如果找不到回傳小於0的整數。
+        // 因為是用binarySearch實做，所以陣列『必須已排序好(遞增)』。
+        int[] array04 = {50,1,-999,6000,77,3,50,0};
+        System.out.println(Arrays.toString(array04));
+        Arrays.sort(array04);
+        System.out.println(Arrays.toString(array04));
+        System.out.println(Arrays.binarySearch(array04, 77));
+        System.out.println(Arrays.binarySearch(array04, 2000));
+
+        // 陣列相等 Arrays.equals(Object[],Object[])
+        // 相等的條件：每個陣列元素的值、順序都完全一樣，
+        // 若 reference 指到同一物件視為相等，都為 null 也視為相等。
+        int[] arrayA = { 5, 6, 7 };
+        int[] arrayB = arrayA.clone();
+        System.out.println(Arrays.equals(arrayA, arrayB));
+        arrayA[0] = 1;
+        System.out.println(Arrays.equals(arrayA, arrayB)); // arrayA變成{1,6,7}，arrayB還是{5,6,7}，不相等
+        arrayA = arrayB;
+        System.out.println(Arrays.equals(arrayA, arrayB));
+        arrayA = null;
+        arrayB = null;
+        System.out.println(Arrays.equals(arrayA, arrayB));
+    }
 }
+
