90e03a261e196b6f4531cbad83f6c75636743a9f
Willium1925 <d1397026@o365.fcu.edu.tw>
2025-03-13 14:05:20 +0800

    可以再試試continue

---
+++ src/com/lee/lesson_sample/basic/Check_0609_CountEven.java
@@ -0,0 +1,23 @@
+package com.lee.lesson_sample.basic;
+
+public class Check_0609_CountEven {
+    public static void main(String[] args) {
+        // 計算指定範圍內的所有偶數和，用while
+        evenTest();
+    }
+
+    static void evenTest() {
+        int min = 0;
+        int max = 10;
+        int i = 0;
+        int sum = 0;
+        while (i <= max) {
+            if (i%2 == 0) {
+                sum = sum + i;
+            }
+            i++;
+        }
+        System.out.println(sum);
+        // 可以試試continue
+    }
+}
