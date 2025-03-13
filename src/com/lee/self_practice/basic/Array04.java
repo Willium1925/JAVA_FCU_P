65cb2da7deabe6d80fdf2529b67a53e8c3764ca1
Willium1925 <d1397026@o365.fcu.edu.tw>
2025-03-13 11:54:21 +0800

    擲骰子和統計-優化

--- src/com/lee/self_practice/basic/Array04.java
+++ src/com/lee/self_practice/basic/Array04.java
@@ -6,6 +6,7 @@ public class Array04 {
     public static void main(String[] args) {
         // 模擬擲兩顆骰子，列出所有點數和的出現次數
         diceTest();
+        diceTest02();
     }
 
     static void diceTest() { // 可行，只是可以更好
@@ -23,6 +24,19 @@ public class Array04 {
                         sumCount[s]++;
                     }
                 }
+            }
+        }
+        System.out.println(Arrays.toString(sumCount));
+    }
+
+    static void diceTest02() {
+        //{2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12} 11種可能
+        //{0, 1, 2, 3, 4, 5, 6, 7,  8,  9, 10} 對應索引值
+        int[] sumCount = new int[11];
+        for (int i = 1; i <=6 ; i++) { // 骰子一
+            for (int j = 1; j <= 6; j++) { // 骰子二
+                // i+j-2 剛好就能對應索引值
+                sumCount[i+j-2]++;
             }
         }
         System.out.println(Arrays.toString(sumCount));
