336ed2e62e7714a19138abe81b3b15a81335e213
Willium1925 <d1397026@o365.fcu.edu.tw>
2025-03-13 14:17:15 +0800

    練習

--- src/com/lee/lesson_sample/basic/Check_0611_Multiplication.java
+++ src/com/lee/lesson_sample/basic/Check_0611_Multiplication.java
@@ -1,6 +1,6 @@
 package com.lee.lesson_sample.basic;
 
-public class Check_0611_Count {
+public class Check_0611_Multiplication {
     public static void main(String[] args) {
 //        doWhile();
 //        normalWhile();
@@ -29,9 +29,12 @@ public class Check_0611_Count {
         // 除非左側是字串，'\t'才會被判斷成tab
         for (int i = 1; i <= 9; i++) {
             //System.out.print(i + '\t');
-            System.out.print(i + "\t");
+            //System.out.print(i + "\t");
             for (int j = 1; j <= 9; j++) {
-                System.out.print(j + "乘" + i + "等於" + j*i + '\t');
+                if (j*i >= 25) {
+                    continue;
+                }
+                System.out.print(j + "乘" + i + "等於" + j*i + "\t");
             }
             System.out.println();
         }
