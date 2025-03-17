f378befc70b824add181d1a2d25f9e8d15308d5b
Willum1925 <D1397026@o365.fcu.edu.tw>
2025-03-17 16:24:10 +0800

    階層

---
+++ src/com/lee/lesson_sample/basic/Check_0623_Factorial.java
@@ -0,0 +1,13 @@
+package com.lee.lesson_sample.basic;
+
+public class Check_0623_Factorial {
+    public static void main(String[] args) {
+        // 計算階層的程式
+        // 指定正整數，輸出該數的階層計算，並詢問是否繼續
+    }
+
+    static void factorialTest00() {
+
+
+    }
+}
