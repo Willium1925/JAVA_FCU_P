fa6664301a84cea60044820363c15a5d2b9f0541
Willum1925 <D1397026@o365.fcu.edu.tw>
2025-03-25 00:46:53 +0800

    封裝，多載練習，建構子
    實例地點，影響使用區域變數or成員變數

--- src/com/lee/self_practice/object_basic/b02_object/CountAvg.java
+++ src/com/lee/self_practice/object_basic/b02_object/CountAvg.java
@@ -67,5 +67,23 @@ public class CountAvg {
         return sum;
     }
 
+    // 建構子
+    // 若你一開始任何建構子都沒寫，JAVA會自動幫妳寫一個不帶任何參數的建構子
+    // 以這個類別來說就像這樣
+    // CountAvg() {
+    // }
+    // 不做任何事，你也看不到，只在其他地方建立實例時(new)派上用場
+    // 但若是你有需求，在其他地方建立實例時(new)，需要同時建立一些初始資料aka傳入參數，
+    // 你就必須自己寫客製化的建構子aka帶參數的建構子
+    // 此時如果你又希望保留乾淨的建構子，就需額外再寫，JAVA不會再幫你偷偷寫了
+    // 而且
+    // 預設情況(自動生成乾淨不做是的建構子)我們寫習慣了，以為"實例化"這件事就只是實例化，一行程式，沒有其他事情發生
+    // 錯!!!
+    // 要是寫成CountAvg() {System.out.print("surprise MDFK")}
+    // 你在某個地方寫CountAvg cag = new CountAvg();時，
+    // console區是會跑出山謬傑克森的啦哈哈哈!!!!!!!
 
+    //CountAvg() {
+    //}
+    CountAvg() { System.out.println("surprise MDFK"); }
 }
