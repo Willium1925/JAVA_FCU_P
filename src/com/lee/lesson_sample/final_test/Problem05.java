7b73a2a1383f9cda9a99f56a41486ff48bb9a718
Willum1925 <D1397026@o365.fcu.edu.tw>
2025-03-26 20:23:17 +0800

    第五題完成
    成績評等判斷

--- src/com/lee/lesson_sample/final_test/Problem05.java
+++ src/com/lee/lesson_sample/final_test/Problem05.java
@@ -1,5 +1,7 @@
 package com.lee.lesson_sample.final_test;
 
+import java.util.Scanner;
+
 // 請讓使用者輸入一個分數
 // 如果大於等於60請輸出pass
 // 若是90分以上，請額外輸出excellent
@@ -13,13 +15,23 @@ package com.lee.lesson_sample.final_test;
 // 90   pass
 public class Problem05 {
     public static void main(String[] args) {
-
+        test01();
     }
 
     static void test01() {
+        Scanner sc = new Scanner(System.in);
+        int insert = sc.nextInt();
 
+        if (insert < 50) {
+            System.out.println("fail");
+        }else if (insert < 60) {
+            System.out.println("almost pass");
+        }else if (insert < 90) {
+            System.out.println("pass");
+        }else if (insert < 100) {
+            System.out.println("pass\nexcellent");
+        }
     }
-
 }
 
 
