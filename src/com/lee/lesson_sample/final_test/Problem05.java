d881f0dce8597aedd90672fb20912a41bd027434
Willium1925 <d1397026@o365.fcu.edu.tw>
2025-03-26 13:26:14 +0800

    第五題

---
+++ src/com/lee/lesson_sample/final_test/Problem05.java
@@ -0,0 +1,25 @@
+package com.lee.lesson_sample.final_test;
+
+// 請讓使用者輸入一個分數
+// 如果大於等於60請輸出pass
+// 若是90分以上，請額外輸出excellent
+// 50~59請輸出almost pass
+// 否則輸出fail
+// Input 輸入一個分數, 輸出等第.
+// Output 如果大於等於60請輸出pass
+// 若是90分以上，請額外輸出excellent
+// 50~59請輸出almost pass
+// 否則輸出fail
+// 90   pass
+public class Problem05 {
+    public static void main(String[] args) {
+
+    }
+
+    static void test01() {
+
+    }
+
+}
+
+
