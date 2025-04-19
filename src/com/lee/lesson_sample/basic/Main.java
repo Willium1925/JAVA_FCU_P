ea2bcde93c8d8f99f61db353080ccf4682c59e47
Willum1925 <D1397026@o365.fcu.edu.tw>
2025-04-19 17:55:15 +0800

    update

--- src/com/lee/lesson_sample/basic/Main.java
+++ src/com/lee/lesson_sample/basic/Main.java
@@ -6,22 +6,22 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //int i = 3;
-        //int j = 4;
-        //i = 3+j+5+6;
-        //j=i;
-        //System.out.println("變數i的內容是" + i);
-        //System.out.println("變數j的內容是" + j);
-        //int i,j,k,l;
-        //i=j=k=l=3+5;
-        //System.out.println("i = " + i);
-        //System.out.println("j = " + j);
-        //System.out.println("k = " + k);
-        //System.out.println("l = " + l);
+//        int i = 3;
+//        int j = 4;
+//        i = 3+j+5+6;
+//        j=i;
+//        System.out.println("變數i的內容是" + i);
+//        System.out.println("變數j的內容是" + j);
+//        int i,j,k,l;
+//        i=j=k=l=3+5;
+//        System.out.println("i = " + i);
+//        System.out.println("j = " + j);
+//        System.out.println("k = " + k);
+//        System.out.println("l = " + l);
 
         //ifTest();
         //ifTest02();
-        //testCheck();
+//        testCheck();
         testString();
     }
 
--- src/com/lee/lesson_sample/final_test/Problem08.java
+++ src/com/lee/lesson_sample/final_test/Problem08.java
@@ -1,5 +1,7 @@
 package com.lee.lesson_sample.final_test;
 
+import java.util.Scanner;
+
 // 肥宅快樂水
 // 在這時快要渴死的你在全聯看到店員正貼著這樣的宣傳單: "三瓶空的可口可樂瓶可以換一瓶可樂"
 // 現在你決定在便利商店買一些可樂(就說N瓶好了), 你想知道你最多可以喝到多少瓶
@@ -21,14 +23,201 @@ package com.lee.lesson_sample.final_test;
 // 13
 public class Problem08 {
     public static void main(String[] args) {
-
+        //test02();
+        //ans01();
+        //ans02();
+        ans03();
+        //ans04();
     }
 
     static void test01() {
-        int bought = 8;
-        int barrow = 0;
-        int drank = 0;
+//        int bought = 8;
+//        int exchange = 0;
+//        int barrow = 0;
+//        int drank = 0;
+//        while ()
+//        drank = (bought/3) + bought%3+1;
 
-        drank = (bought/3) + bought%3+1;
+//        8滿 0空        8
+//            8空
+//        2滿 2空
+//        2滿 2空+1借空
+//        3滿            3
+//            3空
+//        1滿            1
+//            1空
+        Scanner sc = new Scanner(System.in);
+            int bfull = sc.nextInt();
+            int bempt = 0;
+            int borrow = 0;
+            int count = bfull;
+            while (bfull >= 3) {
+                if (bfull%3 == 2) {
+                    bfull = (bfull/3) + ((bfull%3)+1)/3;
+                    borrow++;
+                    bempt = bfull;
+                } else {
+                    bfull = bfull/3;
+                    bempt = bfull + (bfull%3);
+                }
+                if (bempt == borrow) {
+                    count = count + bfull;
+                    break;
+                }
+                count = count + bfull;
+            }
+            System.out.println(count);
+    }
+
+    static void test02() {
+        Scanner sc = new Scanner(System.in);
+        int bfull = sc.nextInt();
+        int bempt = bfull;
+        int borrow = 0;
+        int count = bfull;
+        while (bempt >= 3) {
+            if (bfull%3 == 2) {
+                bfull = (bfull/3) + ((bfull%3)+1)/3;
+                borrow++;
+                bempt = bfull;
+            } else {
+                bempt = bfull%3;
+                bfull = bfull/3;
+                bempt = bempt + bfull;
+            }
+            if (bempt == borrow) {
+                count = count + bfull;
+                break;
+            }
+            count = count + bfull;
+        }
+        System.out.println(count);
+    }
+
+//        8滿 0空        8
+//            8空
+//        2滿 2空
+//        2滿 2空+1借空
+//        3滿            3
+//            3空
+//        1滿            1
+//            1空
+    static void test03() {
+        Scanner sc = new Scanner(System.in);
+        int full = sc.nextInt();
+        int empty = full;
+        int newfull = 0;
+        int leftempty = 0;
+        int borrow = 0;
+
+        newfull = full/3;
+        leftempty = full%3;
+        if (leftempty == 2) {
+            leftempty = leftempty +1;
+            borrow++;
+        }
+
+
+
+    }
+
+    static void ans01() {
+        Scanner input = new Scanner(System.in);
+        while(true){
+            int b1 = input.nextInt();
+            int orig = b1;
+            if (b1==0) {
+                break;
+            }
+            int b2 = 0;
+            while(b1>2){
+                int newb = b1/3;
+                b2 = b2 + newb;
+                b1 = b1%3;
+                b1 = b1 + newb;
+            }
+            if(b1==2)b2++;
+            System.out.println(orig + b2);
+        }
+    }
+
+    static void ans02() {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            int bfull = sc.nextInt(); // 讀取初始瓶數
+            int count = bfull; // 記錄總共喝了幾瓶
+            int empty = bfull; // 記錄當前的空瓶數
+
+            while (empty >= 3) { // 當有足夠的空瓶可以兌換時
+                int newDrink = empty / 3; // 兌換的新飲料
+                count += newDrink; // 加入總數
+                empty = empty % 3 + newDrink; // 更新剩餘空瓶數
+            }
+
+            // 如果最後剩2個空瓶，可以向店家借1瓶，再喝完還回來
+            if (empty == 2) {
+                count++;
+            }
+
+            System.out.println(count);
+        }
+        sc.close();
+    }
+
+    static void ans03() {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            int bfull = sc.nextInt(); // 初始滿瓶數
+
+            if (bfull <= 0 || bfull > 200) {
+                break;
+            }
+            int count = bfull; // 紀錄喝了幾瓶
+            int empty = bfull; // 記錄當前空瓶數
+
+            while (empty >= 3) { // 當有足夠的空瓶可以兌換時
+                int newDrink = empty / 3; // 兌換的新滿瓶
+                count += newDrink; // 新的喝光，加進計數
+                empty = empty % 3 + newDrink; // 更新剩餘空瓶數
+            }
+
+            // 剩2瓶再借，喝完剛好還回來
+            if (empty == 2) {
+                count++;
+            }
+
+            System.out.println(count);
+        }
+        sc.close();
+    }
+
+    static void ans04() {
+        Scanner sc=new Scanner(System.in);
+        while (true) {
+            int a = sc.nextInt();
+            if (a >= 1 && a <= 200) {
+                int sum = a;
+                a++;
+                while (a > 0) {
+
+                    int b = a / 3;
+                    int c = a % 3;
+                    sum += b;
+
+                    b += c;
+
+                    if (b >= 3) {
+                        a = b;
+                    } else if (b < 3) {
+                        a = 0;
+                    }
+                }
+                System.out.println(sum);
+
+            }else {
+                break;
+            }
+
+        }
     }
 }
